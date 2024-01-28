f3add739e013ca967c15d0de8ca09b7639ea6d94
vinisha <vinishadv@gmail.com>
2024-01-28 22:20:17 +0530

    minorChange

--- app/ui/home/styles.ts
+++ app/ui/home/styles.ts
@@ -8,7 +8,7 @@ export const styles = {
     position: 'relative'
   },
   description_uploader_wrapper: {
-    mt: { base: rem(120), sm: rem(130), md: rem(90) },
+    mt: { base: rem(120), sm: rem(130), md: rem(100) },
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
