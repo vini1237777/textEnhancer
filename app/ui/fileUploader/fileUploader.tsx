68d56fc58f90a1acfea0877c8bb180edc036988f
vinisha <vinishadv@gmail.com>
2024-01-29 20:45:03 +0530

    minorChange

--- app/ui/fileUploader/fileUploader.tsx
+++ app/ui/fileUploader/fileUploader.tsx
@@ -202,7 +202,7 @@ const FileUploader = () => {
         {showResult && isdataAvailable && <Result data={data || {}} />}
       </Box>
       {/* Component for uploading sample documents */}
-      {!loading && !isdataAvailable && error && (
+      {!loading && !isdataAvailable && (
         <SampleDocUploader fetchSampleFileData={fetchData} />
       )}
     </>
